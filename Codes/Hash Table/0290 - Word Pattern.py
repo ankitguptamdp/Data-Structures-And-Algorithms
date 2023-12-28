0cffa63ab2532fd22bfed34c971c7404ccecaa47
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2023-12-28 13:34:51 +0530

    Adding Word Pattern

---
+++ Codes/Hash Table/0290 - Word Pattern.py
@@ -0,0 +1,19 @@
+# Time Complexity: O(n+m)
+# Space Complexity: O(n+m)
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+
+        charToWord = {}
+        wordToChar = {}
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
+        return True
