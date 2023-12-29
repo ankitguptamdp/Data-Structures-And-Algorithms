b2f1676f79e42db8c288481af3e88ccb3108aac0
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2023-12-29 13:11:26 +0530

    Adding Contains Duplicate II Link

--- Codes/Array/0026 - Remove Duplicates from Sorted Array.py
+++ Codes/Array/0026 - Remove Duplicates from Sorted Array.py
@@ -1,9 +1,12 @@
+# Difficulty: Easy
+# Status: Solved
+# Tags: Array, Two Pointers
+# Time Complexity: O(n)
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         prev = 0
         for index in range(len(nums)):
-            if nums[index] != nums[prev]:
+            if nums[prev] != nums[index]:
                 prev += 1
                 nums[prev] = nums[index]
-
         return prev + 1
