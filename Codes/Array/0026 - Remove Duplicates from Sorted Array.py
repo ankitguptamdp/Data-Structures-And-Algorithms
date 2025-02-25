8552601c6a23e2548b4d0cedc88599cac52b6f30
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-02-25 08:57:35 +0000

    Adding Solution For The Problem 0027 Remove Element

--- Codes/Array/0026 - Remove Duplicates from Sorted Array.py
+++ Codes/Array/0026 - Remove Duplicates from Sorted Array.py
@@ -1,7 +1,10 @@
+# Last Updated: 25-02-2025 - 08:53 AM
 # Difficulty: Easy
 # Status: Solved
 # Tags: Array, Two Pointers
 # Time Complexity: O(n)
+# Space Complexity: O(1)
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         prev = 0
--- Codes/Array/0027 - Remove Element.py
+++ Codes/Array/0027 - Remove Element.py
@@ -1,11 +1,14 @@
+# Difficulty: Easy
+# Status: Solved
+# Tags: Array, Two Pointers
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+        prev = 0
+        for index in range(len(nums)):
+            if nums[index] != val:
+                nums[prev] = nums[index]
+                prev += 1
+        return prev
