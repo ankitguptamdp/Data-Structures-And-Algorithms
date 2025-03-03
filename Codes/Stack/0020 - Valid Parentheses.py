f4a9e687d657c85ea0ece3442b1d8c8a0dc0f1cb
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-03-03 22:53:41 +0000

    Adding Solution For The Problem 0020 Valid Parentheses

--- Codes/Stack/0020 - Valid Parentheses.py
+++ Codes/Stack/0020 - Valid Parentheses.py
@@ -1,19 +1,24 @@
+# Last Updated: 03-03-2025 - 10:52 PM
+# Difficulty: Easy
+# Status: Solved
+# Tags: String, Stack
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         closeToOpen = {
             ")": "(",
+            "}": "{",
             "]": "[",
-            "}": "{"
         }
-
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+        for character in s:
+            if character in closeToOpen:
+                if stack and stack[-1] == closeToOpen[character]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(c)
-                
+                stack.append(character)
         return True if not stack else False
