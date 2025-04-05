4533bd5f88be8fcf3ce6f9ef6b608d6299b44695
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-05 21:47:33 +0100

    Adding Bit Manipulation Solution To The Problem 1470

--- Codes/Array/1470 - Shuffle The Array.py
+++ Codes/Array/1470 - Shuffle The Array.py
@@ -13,3 +13,16 @@ class Solution:
             output.append(nums[index])
             output.append(nums[n + index])
         return output
+
+# Bit Manipulation
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        for index in range(n):
+            nums[index] = nums[index] << 10
+            nums[index] = nums[index] | nums[index+n]
+        for index in range(n):
+            nums[-2*index-1] = nums[n-index-1] & (2**10-1)
+            nums[-2*index-2] = nums[n-index-1] >> 10
+        return nums
