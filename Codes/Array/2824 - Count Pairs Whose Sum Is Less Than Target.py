22bb9d1131b1741b96c3bc527eed13ffa9cfc815
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-11 22:52:36 +0100

    Adding Solution For The Problem 2824

--- Codes/Array/2824 - Count Pairs Whose Sum Is Less Than Target.py
+++ Codes/Array/2824 - Count Pairs Whose Sum Is Less Than Target.py
@@ -14,3 +14,20 @@ class Solution:
                 if nums[i] + nums[j] < target:
                     count += 1
         return count
+
+# Two Pointers
+# Time Complexity: O(n*ln(n))
+# Space Complexity: O(1)
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        nums = sorted(nums)
+        l = 0
+        r = len(nums) - 1
+        while l != r:
+            if nums[l] + nums[r] < target:
+                count += r-l
+                l += 1
+            else:
+                r -= 1
+        return count
