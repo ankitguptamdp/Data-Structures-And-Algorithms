e4b923b3ca83bd437e228b83cafa89cea16af9af
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-12 20:02:46 +0100

    Adding Solution For The Problem 2574

--- Codes/Array/2574 - Left And Right Sum Differences.py
+++ Codes/Array/2574 - Left And Right Sum Differences.py
@@ -1,4 +1,4 @@
-# Last Updated: 12-04-2025 - 07:26 PM
+# Last Updated: 12-04-2025 - 07:53 AM
 # Difficulty: Easy
 # Status: Solved
 # Tags: Array, Prefix Sum
@@ -16,3 +16,17 @@ class Solution:
             rightSum[i] = rightSum[i + 1] + nums[i + 1]
         output = [abs(leftSum[i] - rightSum[i]) for i in range(len(nums))]
         return output
+
+# Prefix Sum
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        sm = sum(nums)
+        leftSum = 0
+        for i in range(len(nums)):
+            num = nums[i]
+            rightSum = sm - leftSum - num
+            nums[i] = abs(leftSum - rightSum)
+            leftSum += num
+        return nums
