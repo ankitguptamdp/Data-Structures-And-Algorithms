3f25503efb0fb3d0c3c552a680bbe3b7b3565dd5
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-12 23:16:41 +0100

    Adding Counting Sort Solution For The Problem 1365

---
+++ Codes/Array/1365 - How Many Numbers Are Smaller Than The Current Number.py
@@ -0,0 +1,21 @@
+# Last Updated: 12-04-2025 - 09:11 AM
+# Difficulty: Easy
+# Status: Solved
+# Tags: Array, Counting Sort, Hash Table, Sorting
+
+# Counting Sort
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        count = [0] * (max(nums) + 1)
+        for num in nums:
+            count[num] += 1
+        leftSum = 0
+        for i in range(len(count)):
+            cnt = count[i]
+            count[i] = leftSum
+            leftSum += cnt
+        for i in range(len(nums)):
+            nums[i] = count[nums[i]]
+        return nums
