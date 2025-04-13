cdd363955138881f293c838b5c5fbbeff4286198
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-13 22:56:14 +0100

    Adding Solution And Markdown For The Problem 1720

--- Codes/Array/1720 - Decode XORed Array.py
+++ Codes/Array/1720 - Decode XORed Array.py
@@ -5,10 +5,18 @@
 
 # Brute Force
 # Time Complexity: O(n)
-# Space Complexity: O(1)
+# Space Complexity: O(n)
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
         encoded[0] ^= first
         for i in range(1, len(encoded)):
             encoded[i] ^= encoded[i - 1]
         return [first] + encoded
+
+# Another Solution
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        o = [first]
+        for i in encoded:
+            o.append(i ^ o[-1])
+        return o
