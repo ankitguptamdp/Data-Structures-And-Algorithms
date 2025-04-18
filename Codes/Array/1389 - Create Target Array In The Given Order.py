354b0bec7a043440b3fb09d174875527166ab108
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-18 21:12:51 +0100

    Adding Solution And Markdown For The Problem 1389

--- Codes/Array/1389 - Create Target Array In The Given Order.py
+++ Codes/Array/1389 - Create Target Array In The Given Order.py
@@ -11,3 +11,30 @@ class Solution:
         for i in range(len(nums)):
             target.insert(index[i], nums[i])
         return target
+
+# Another Solution
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+        for i in range(len(nums)):
+            if len(target) == index[i]:
+                target.append(nums[i])
+            else:
+                target.insert(index[i], nums[i])
+        return target
+
+# Without Insert Function
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+        for i in range(len(nums)):
+            if len(target) == index[i]:
+                target.append(nums[i])
+            else:
+                target = target[: index[i]] + [nums[i]] + target[index[i] :]
+        return target
+
