affb921a20c610ac79ab6e0fd2b292be27ef1239
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-18 22:14:43 +0100

    Adding Solution For The Problem 1313

---
+++ Codes/Array/1313 - Decompress Run Length Encoded List.py
@@ -0,0 +1,15 @@
+# Last Updated: 18-04-2025 - 10:13 PM
+# Difficulty: Easy
+# Status: Solved
+# Tags: Array
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        output = []
+        for i in range(0, len(nums), 2):
+            val = nums[i + 1]
+            freq = nums[i]
+            output += [val] * freq
+        return output
