d3217e0ab0a66875bbafc64e83e4af44bd695d36
Ankit Kumar Gupta <ankitguptamdp@gmail.com>
2025-04-20 22:49:59 +0100

    Adding Prefix Sum Solution And Markdown For The Problem 3427

--- Codes/Array/3427 - Sum Of Variable Length Subarrays.py
+++ Codes/Array/3427 - Sum Of Variable Length Subarrays.py
@@ -3,6 +3,7 @@
 # Status: Solved
 # Tags: Array, Prefix Sum
 
+# Brute Force
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 class Solution:
@@ -12,3 +13,17 @@ class Solution:
             start = max(0, i - nums[i])
             output += sum(nums[start : i + 1])
         return output
+
+# Prefix Sum
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        result = nums[0]
+        for i in range(1, len(nums)):
+            start = i - nums[i]
+            nums[i] += nums[i - 1]
+            result += nums[i]
+            if start - 1 >= 0:
+                result -= nums[start - 1]
+        return result
